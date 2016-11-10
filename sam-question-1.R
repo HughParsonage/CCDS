10b48537aff9bc421d14c2536dac073b0d0cf18a
Sam Villella <villella.sam@gmail.com>
2016-11-10 19:42:36 +1100

    Closes

--- sam-question-1.R
+++ sam-question-1.R
@@ -1,2 +1,10 @@
 # 1 -- Max number of chars in a line
-pri
+
+# split of whitespace
+matrix <- strsplit(prideprejudice, " ", TRUE)
+
+#flatten matrix(2D) to a vector(1D)
+vector1 <- unlist(matrix)
+
+#try and regroup then into groups of same word
+split(vector1, f = 1)
